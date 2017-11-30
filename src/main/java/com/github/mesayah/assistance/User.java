ed902169f4d1afd0d9c37bb0cb31d044665cc637
Przemysław Szyk <przemek600032@gmail.com>
2017-11-30 12:00:35 +0100

    Created User class and defined its attributes. ASSISTANCE-34 In Progress

---
+++ src/main/java/com/github/mesayah/assistance/User.java
@@ -0,0 +1,68 @@
+package com.github.mesayah.assistance;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String user;
+    private String role;
+    private String firstName;
+    private String lastName;
+
+    public User(long id, String user, String role, String firstName, String lastName) {
+        this.id = id;
+        this.user = user;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
