f93826bb8a1877d22bd928588633fe7097299b22
Przemysław Szyk <przemek600032@gmail.com>
2017-12-08 18:03:35 +0100

    Created chat message service class and implemented it's CRUD operations ASSISTANCE-136 Completed

---
+++ src/main/java/com/github/mesayah/assistance/projects/ChatMessageService.java
@@ -0,0 +1,95 @@
+package com.github.mesayah.assistance.projects;
+
+import com.github.mesayah.assistance.model.Channel;
+import com.github.mesayah.assistance.model.ChatMessage;
+import com.github.mesayah.assistance.repository.ChatMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Service for managing chat messages.
+ */
+@Service
+public class ChatMessageService {
+    /**
+     * Repository fetching chat messages from database.
+     */
+    @Autowired
+    ChatMessageRepository chatMessageRepository;
+
+    /**
+     * Default constructor.
+     */
+    public ChatMessageService() {
+    }
+
+    /**
+     * Saves a given chat message entity in database.
+     *
+     * @param chatMessage chat massage entity to save.
+     */
+    public void save(ChatMessage chatMessage) {
+        chatMessageRepository.save(chatMessage);
+    }
+
+    /**
+     * Retrieves a chat message entity by its id.
+     * @param id given id
+     * @return a chat message entity with the given id or null if none found.
+     */
+    public ChatMessage findById(Long id)
+    {
+        return chatMessageRepository.findOne(id);
+    }
+
+    /**
+     * Retrieves all chat message entities in the given channel.
+     * @param channel given channel entity.
+     * @return chat message entities in the given channel or null if none found.
+     */
+    public List<ChatMessage> findAllByChannel(Channel channel)
+    {
+        return chatMessageRepository.findAllByChannel(channel);
+    }
+
+    /**
+     * Retrieves all chat message entities in the given channel at a given date.
+     * @param channel given channel entity.
+     * @param sendDate given date.
+     * @return entities in the given channel at a given date or null if none found.
+     */
+    public List<ChatMessage> findAllByChannelAndSendDate(Channel channel, Date sendDate)
+    {
+        return chatMessageRepository.findAllByChannelAndSendDate(channel,sendDate);
+    }
+
+    /**
+     * Deletes the chat message entity with the given id.
+     * @param id given id.
+     */
+    public void delete(Long id)
+    {
+        chatMessageRepository.delete(id);
+    }
+
+    /**
+     * Deletes all chat message entities in given channel.
+     * @param channel given channel.
+     */
+    public void delete(Channel channel)
+    {
+        chatMessageRepository.deleteAllByChannel(channel);
+    }
+
+    /**
+     * Deletes all chat message entities before given date.
+     * @param sendDate given date.
+     */
+    public void delete(Date sendDate)
+    {
+        chatMessageRepository.deleteAllBySendDateBefore(sendDate);
+    }
+}
--- src/main/java/com/github/mesayah/assistance/repository/ChatMessageRepository.java
+++ src/main/java/com/github/mesayah/assistance/repository/ChatMessageRepository.java
@@ -1,7 +1,15 @@
 package com.github.mesayah.assistance.repository;
 
+import com.github.mesayah.assistance.model.Channel;
 import com.github.mesayah.assistance.model.ChatMessage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ChatMessageRepository extends CrudRepository<ChatMessage, Long> {
+    List<ChatMessage> findAllByChannel(Channel channel);
+    List<ChatMessage> findAllByChannelAndSendDate(Channel channel, Date sendDate);
+    void deleteAllByChannel(Channel channel);
+    void deleteAllBySendDateBefore(Date sendDate);
 }
