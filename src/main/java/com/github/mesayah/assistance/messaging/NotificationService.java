96d51e52d8db116ac05b5a64b26cb8c4e9b821d8
Mesayah <f.piechowski@gmail.com>
2017-12-09 13:19:16 +0100

    Created Notification service class. #ASSISTANCE-146 In Progress

---
+++ src/main/java/com/github/mesayah/assistance/messaging/NotificationService.java
@@ -0,0 +1,12 @@
+package com.github.mesayah.assistance.messaging;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for sending scheduled notifications.
+ */
+@Service
+public class NotificationService {
+
+
+}
