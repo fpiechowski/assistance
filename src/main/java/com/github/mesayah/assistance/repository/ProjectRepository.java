07061c0298be8524bbeaee85198e601fc3ddee03
Przemysław Szyk <przemek600032@gmail.com>
2017-12-11 08:28:38 +0100

    Created project service class and implemented it's CRUD operations ASSISTANCE-114 Completed

--- src/main/java/com/github/mesayah/assistance/repository/ProjectRepository.java
+++ src/main/java/com/github/mesayah/assistance/repository/ProjectRepository.java
@@ -1,10 +1,14 @@
 package com.github.mesayah.assistance.repository;
 
 import com.github.mesayah.assistance.model.Project;
+import com.github.mesayah.assistance.model.Status;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Repository for fetching and saving projects in the database. It handles database communication.
  */
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    List<Project> findAllByStatus(Status status);
 }
