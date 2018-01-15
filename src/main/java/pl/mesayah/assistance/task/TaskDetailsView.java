ba286096fe5547c340a6c338d5ffd5677d0b45f9
Przemysław Szyk <przemek600032@gmail.com>
2018-01-15 15:36:08 +0100

    Created TaskDetailsView and added some of its content

---
+++ src/main/java/pl/mesayah/assistance/task/TaskDetailsView.java
@@ -0,0 +1,302 @@
+package pl.mesayah.assistance.task;
+
+import com.vaadin.data.Binder;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.mesayah.assistance.AbstractDetailsView;
+import pl.mesayah.assistance.messaging.Channel;
+import pl.mesayah.assistance.messaging.ChannelRepository;
+import pl.mesayah.assistance.project.Project;
+import pl.mesayah.assistance.project.ProjectRepository;
+import pl.mesayah.assistance.user.User;
+import pl.mesayah.assistance.user.UserRepository;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A view of task details. It presents parameters of a given task.
+ */
+@SpringView(name = TaskDetailsView.VIEW_NAME)
+public class TaskDetailsView extends AbstractDetailsView<Task> {
+
+    /**
+     * A view name used to navigate between views.
+     */
+    public static final String VIEW_NAME = "task";
+
+    /**
+     * A label showing a title of the task.
+     */
+    private Label nameLabel;
+
+    /**
+     * A Text Field to edit a title of the task.
+     */
+    private TextField nameTextField;
+
+    /**
+     * A label showing a status of the task.
+     */
+    private Label statusLabel;
+
+    /**
+     * A drop down menu to select a status of the task.
+     */
+    private NativeSelect<Task.Status> statusNativeSelect;
+    /**
+     * A label showing a priority of the task.
+     */
+    private Label assignedUsersLabel;
+    /**
+     * A drop down menu to select a priority of the task.
+     */
+    private ComboBox<User> assignedUsersNativeSelect;
+    /**
+     * A label showing a priority of the task.
+     */
+    private Label priorityLabel;
+    /**
+     * A drop down menu to select a priority of the task.
+     */
+    private NativeSelect<Task.Priority> priorityNativeSelect;
+    /**
+     * A label showing a channel of the task.
+     */
+    private Label parentTaskLabel;
+    /**
+     * A combo box for selecting task channel.
+     */
+    private ComboBox<Task> parentTaskComboBox;
+    /**
+     * A label showing a channel of the task.
+     */
+    private Label subtasksLabel;
+    /**
+     * A combo box for selecting task channel.
+     */
+    private ComboBox<Task> subtasksComboBox;
+    /**
+     * A label showing a channel of the task.
+     */
+    private Label channelLabel;
+    /**
+     * A combo box for selecting task channel.
+     */
+    private ComboBox<Channel> channelComboBox;
+    /**
+     * A label showing a type of the task.
+     */
+    private Label typeLabel;
+    /**
+     * A drop down menu to select a type of the task.
+     */
+    private NativeSelect<Task.Type> typeNativeSelect;
+    /**
+     * A label showing a project of the task.
+     */
+    private Label projectLabel;
+    /**
+     * A combo box for selecting task project.
+     */
+    private ComboBox<Project> projectComboBox;
+    /**
+     * A label showing a description of the task.
+     */
+    private Label descriptionLabel;
+
+    /**
+     * A text area for editing task's description.
+     */
+    private TextArea descriptionTextArea;
+
+    /**
+     * A label showing a task deadline.
+     */
+    private Label deadlineLabel;
+
+    /**
+     * A date field for selecting task's deadline date.
+     */
+    private DateField deadlineDateField;
+
+    /**
+     * A unified formatter for start and deadline dates.
+     */
+    private DateTimeFormatter dateTimeFormatter;
+
+    //@Autowired
+    //private RoleService roleService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private ChannelRepository channelRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    /**
+     * Constructs a view in a read mode and initializes controls for it.
+     */
+    public TaskDetailsView() {
+
+        dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM dd yyyyy");
+    }
+
+    @Override
+    protected List<Component> initializeReadComponents() {
+
+        nameLabel = new Label();
+        nameLabel.setCaption("Name: ");
+        nameLabel.setWidth("100%");
+
+        statusLabel = new Label();
+        statusLabel.setCaption("Status:");
+
+        HorizontalLayout readNameStatusLayout = new HorizontalLayout(nameLabel, statusLabel);
+        readNameStatusLayout.setWidth("100%");
+
+
+        descriptionLabel = new Label();
+        descriptionLabel.setCaption("Description:");
+        descriptionLabel.setWidth("100%");
+
+        priorityLabel = new Label();
+        priorityLabel.setCaption("Priority:");
+        priorityLabel.setWidth("100%");
+
+        deadlineLabel = new Label();
+        deadlineLabel.setCaption("Deadline:");
+
+        assignedUsersLabel = new Label();
+        assignedUsersLabel.setCaption("User");
+
+        HorizontalLayout readDatesLayout = new HorizontalLayout(deadlineLabel,assignedUsersLabel);
+        readDatesLayout.setWidth("100%");
+
+        return new ArrayList<>(Arrays.asList(
+                readNameStatusLayout,
+                descriptionLabel,
+                readDatesLayout
+        ));
+    }
+
+    @Override
+    protected List<Component> initializeEditComponents() {
+
+        nameTextField = new TextField("Name:");
+        nameTextField.setWidth("100%");
+        nameTextField.setRequiredIndicatorVisible(true);
+
+        statusNativeSelect = new NativeSelect<>("Status:",
+                Arrays.asList(Task.Status.values()));
+        statusNativeSelect.setEmptySelectionAllowed(false);
+        statusNativeSelect.setSelectedItem(Task.Status.WAITING);
+        statusNativeSelect.setWidth("200px");
+        statusNativeSelect.setRequiredIndicatorVisible(true);
+
+        priorityNativeSelect = new NativeSelect<>("Priority:",
+                Arrays.asList(Task.Priority.values()));
+        priorityNativeSelect.setEmptySelectionAllowed(false);
+        priorityNativeSelect.setSelectedItem(Task.Priority.MEDIUM);
+        priorityNativeSelect.setWidth("200px");
+        priorityNativeSelect.setRequiredIndicatorVisible(true);
+        HorizontalLayout editNameStatusLayout = new HorizontalLayout(nameTextField, statusNativeSelect, priorityNativeSelect);
+
+
+        descriptionTextArea = new TextArea("Description:");
+        descriptionTextArea.setWidth("100%");
+
+        deadlineDateField = new DateField("Deadline:");
+
+        HorizontalLayout editDatesLayout = new HorizontalLayout(deadlineDateField);
+
+        return new ArrayList<>(Arrays.asList(
+                editNameStatusLayout,
+                descriptionTextArea,
+                editDatesLayout
+        ));
+    }
+
+    @Override
+    protected Button initializeDeleteButton() {
+
+        return new Button("Delete", VaadinIcons.TRASH);
+    }
+
+    @Override
+    protected Button initializeConfirmButton() {
+
+        return new Button("Confirm", VaadinIcons.CHECK);
+    }
+
+    @Override
+    protected Button initializeEditButton() {
+
+        return new Button("Edit", VaadinIcons.PENCIL);
+    }
+
+    protected Binder<Task> initializeDataBinder() {
+
+        Binder<Task> dataBinder = new Binder<>(Task.class);
+
+        dataBinder.forField(nameTextField)
+                .withValidator(name -> name.length() > 0, "Name must not be empty.")
+                .bind(Task::getName, Task::setName);
+        dataBinder.forField(statusNativeSelect)
+                .bind(Task::getStatus, Task::setStatus);
+        dataBinder.forField(priorityNativeSelect)
+                .bind(Task::getPriority, Task::setPriority);
+        dataBinder.forField(descriptionTextArea)
+                .bind(Task::getDescription, Task::setDescription);
+        dataBinder.forField(deadlineDateField)
+                .bind(Task::getDeadline, Task::setDeadline);
+
+        return dataBinder;
+    }
+
+    @Override
+    protected void loadData() {
+
+//        Collection<User> projectManagers = roleService.findByName(Role.PROJECT_MANAGER).getUsers();
+//        managerComboBox.setItems(projectManagers);
+    }
+
+    @Override
+    public Task createEmptyEntity() {
+
+        Task empty = new Task();
+
+        return empty;
+    }
+
+    @Override
+    protected void setReadComponentsValues() {
+
+        nameLabel.setValue(getEntity().getName());
+
+        statusLabel.setValue(getEntity().getStatus().toString());
+
+        priorityLabel.setValue(getEntity().getPriority().toString());
+
+        descriptionLabel.setValue(getEntity().getDescription());
+
+        // Deadline can be empty (can be null) so we need to check it before setting a label's value.
+        String deadline;
+        if (getEntity().getDeadline() == null) {
+            deadline = null;
+        } else {
+            deadline = getEntity().getDeadline().format(dateTimeFormatter);
+        }
+        deadlineLabel.setValue(deadline);
+    }
+}
